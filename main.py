5dd60ec1d6f887117a148995d0de172ebff83ab7
josiah-brown <josiahbrown321@gmail.com>
2021-08-16 19:31:15 +0700

    Fix Postgres bug

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-if os.environ.get("DATABASE_URL1"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+# if os.environ.get("DATABASE_URL1"):
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
